29684c43ebef05941e5c38eea972c8a89388bccb
oabraham <omar.abraham@upr.edu>
2018-02-27 19:56:06 -0400

    sec

--- Main/main.c
+++ Main/main.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+#include<time.h>
 
 int main(void){
-	printf("hey");
+	clock_t startTime = (float)clock()/CLOCKS_PER_SEC;
+
+	for(int a=0; a<100000; a++){
+		for(int b=0; b<100000; b++){
+			int meh = a + b;
+		}
+	}
+	clock_t endTime = (float)clock()/CLOCKS_PER_SEC;
+	clock_t timeElapsed = endTime - startTime;
+	printf("The time: %ld", timeElapsed);
 	return 0;
 }
-
